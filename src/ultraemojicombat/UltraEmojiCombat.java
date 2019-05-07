5fe0cf346f1c00b66b28c0d78fbe52605e6062ab
J4R84S <jarbasstm@hotmail.com>
2019-05-06 22:53:29 -0300

    Atualizado em 06052019

---
+++ src/ultraemojicombat/UltraEmojiCombat.java
@@ -0,0 +1,18 @@
+
+package ultraemojicombat;
+
+
+public class UltraEmojiCombat {
+
+    
+    public static void main(String[] args) {
+        
+       Lutador l[] = new Lutador[6]; // Classe Vetor
+               
+       l[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2,1);
+       
+       l[0].apresentar();
+       
+    }
+    
+}
